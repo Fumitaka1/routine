5268e9c5b6bbe28b3d41611c2cda9840afd344a3
fumitaka <f.sato0130@gmail.com>
2020-05-11 08:00:04 +0000

    deviseを導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    
+  }
+    
+  get '/user/:id', to: 'users/show#show'
   root 'static_pages#home'
 end
