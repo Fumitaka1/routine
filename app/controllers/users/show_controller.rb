188bf6668760bf397bea3929e244441aa2e847f2
f.sato <f.sato0130@gmail.com>
2020-05-19 20:15:57 +0900

    ユーザーフォロー機能を作成

--- app/controllers/users/show_controller.rb
+++ app/controllers/users/show_controller.rb
@@ -3,4 +3,18 @@ class Users::ShowController < ApplicationController
     @user = User.find(params[:id])
     @posts = Post.where(user_id: @user.id).paginate(page: params[:page], per_page: 20)
   end
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
 end
